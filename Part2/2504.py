49d9933b6cd1a5c582722a3bf754c4dd8d6a2d50
mallang7 <seonug0725@gmail.com>
2021-07-21 20:16:30 +0900

    .

---
+++ Part2/2504.py
@@ -0,0 +1,53 @@
+#2504번 - 괄호의 값
+str = input().rstrip()
+stack=list()
+
+for i in str:
+    if i == ")":
+        temp = 0
+        while stack:
+            top = stack.pop()
+            if top == "(":
+                if temp == 0:
+                    stack.append(2)
+                else:
+                    stack.append(2*temp)
+                break
+            elif top == "[":
+                print("0")
+                exit(0)
+            else:
+                if temp == 0:
+                    temp = int(top)
+                else:
+                    temp += int(top)
+    elif i == "]":
+        temp = 0
+        while stack:
+            top = stack.pop()
+            if top == "[":
+                if temp == 0:
+                    stack.append(3)
+                else:
+                    stack.append(3*temp)
+                break
+            elif top == "(":
+                print("0")
+                exit(0)
+            else:
+                if temp == 0:
+                    temp = int(top)
+                else:
+                    temp += int(top)
+    else:
+        stack.append(i)
+        
+result = 0
+for i in stack:
+    if i == "(" or i == "[":
+        print(0)
+        exit(0)
+    else:
+        result += i
+print(result)
+            
