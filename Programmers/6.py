57d329b521989b0ce8d214dadee498b46e462b66
mallang7 <seonug0725@gmail.com>
2021-08-02 14:02:22 +0900

    Create 6.py

---
+++ Programmers/6.py
@@ -0,0 +1,17 @@
+# 오픈채팅방
+def solution(record):
+    answer = []
+    dic ={}
+    
+    for sentence in record:
+        sentence_split = sentence.split()
+        if len(sentence_split) == 3:
+            dic[sentence_split[1]] = sentence_split[2]
+    for sentence in record:
+        sentence_split = sentence.split()
+        if sentence_split[0] == 'Enter':
+            answer.append('{}님이 들어왔습니다.'.format(dic[sentence_split[1]]))
+        elif sentence_split[0] == 'Leave':
+            answer.append('{}님이 나갔습니다.'.format(dic[sentence_split[1]]))
+    
+    return answer
