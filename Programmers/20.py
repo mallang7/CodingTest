6a26c405367d2c379ca4ca2c68ea661992d4def8
mallang7 <seonug0725@gmail.com>
2021-10-21 22:19:24 +0900

    Create 20.py

---
+++ Programmers/20.py
@@ -0,0 +1,16 @@
+# 소수 만들기
+from itertools import combinations
+def check(a,b,c):
+    total = a + b + c
+    for i in range(2,total):
+        if total % i == 0:
+            return False
+    return True
+        
+def solution(nums):
+    answer = 0
+    comb=list(combinations(nums, 3))
+    for i in comb:
+        if check(i[0],i[1],i[2]):
+            answer += 1
+    return answer
