1dad1ee17583842bb77215a533af85e6e40cbfcb
mallang7 <seonug0725@gmail.com>
2022-01-18 23:50:09 +0900

    Update 1_1.py

--- Algo_book/02_implement/1_1.py
+++ Algo_book/02_implement/1_1.py
@@ -1,1 +1,19 @@
 # 구현 - 1. 상하좌우
+N = int(input())
+datas=list(input().split())
+x=1
+y=1
+for data in datas:
+    if(data=='L'):
+        if (y-1 != 0):
+            y -= 1
+    elif(data=='R'):
+        if (y+1 != N):
+            y += 1
+    elif(data=='U'):
+        if (x-1 != 0):
+            x -= 1
+    else: #data==D
+        if (x+1 != N):
+            x += 1
+print('{} {}'.format(x,y))
