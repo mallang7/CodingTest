2f0c68e2061b9ee2b35880d1e3068efd0035ff5d
mallang7 <seonug0725@gmail.com>
2022-01-21 20:33:11 +0900

    Update 1_3.py

--- Algo_book/02_implement/1_3.py
+++ Algo_book/02_implement/1_3.py
@@ -1,5 +1,13 @@
 # 구현 - 왕실의 나이트 115p
 A = input()
-count = 0
-#for i in range(4): #왼,오,위,아래 총 4번
-y=A[0], x=A[1] # y=1, x=a
+x=ord('a')-ord(A[0])+1
+y=int(A[1])
+
+datas=[(-2,-1),(-2,1),(-1,2),(-1,-2),(1,2),(1,-2),(2,1),(2,-1)]
+count=0
+for data in datas:
+    next_row= x + data[0]
+    next_column= y + data[1]
+    if (next_row >=1 and next_row <=8) and (next_column >=1 and next_column <=8):
+        count += 1
+print(count)
