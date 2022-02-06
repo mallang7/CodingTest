27cbc5a715e736f3733bd2d2358c18042c534938
mallang7 <seonug0725@gmail.com>
2022-02-06 19:27:54 +0900

    Create 1_1.py

---
+++ Algo_book/03_DFS/BFS/1_1.py
@@ -0,0 +1,29 @@
+# 음료수 얼려 먹기 149p
+n, m = map(int,input().split())
+
+graph=[]
+for i in range(n):
+    graph.append(list(map(int,input())))
+    
+print(graph)
+
+def dfs(x,y):
+    #경로이탈시 종료
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if graph[x][y]==0: #방문하지 않았으면
+        graph[x][y] = 1
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x,y-1)
+        dfs(x,y+1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j)==True:
+            result += 1
+
+print(result)
