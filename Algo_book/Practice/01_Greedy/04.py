be5f7f566cfd562c6a1ffe72d8f6ee134ede9e1a
mallang7 <seonug0725@gmail.com>
2022-02-13 19:29:12 +0900

    Update 04.py

--- Algo_book/Practice/01_Greedy/04.py
+++ Algo_book/Practice/01_Greedy/04.py
@@ -1,27 +1,11 @@
-from itertools import combinations
 N = int(input())
 coins=list(map(int,input().split()))
-
 coins.sort()
-money=coins.copy()
-for i in range(2,len(coins)):
-    money+=list(combinations(coins,i))
-print(money)
 
-sum_li=[]
-for i in range(len(money)):
-    sum_li.append(sum(money[i]))
+target = 1
+for i in coins:
+    if target < i:
+        break
+    target += i
 
-print(sum_li)
-    
-    
-count = 0  
-while True:
-    count += 1
-    for i in money:
-        if count == sum(i):
-            continue
-        else:
-            break
-
-print(count)
+print(target)
