520976f9a06764a5de16457da716c94fe0a7ddf6
mallang7 <seonug0725@gmail.com>
2022-02-19 19:32:55 +0900

    Update 05.py

--- Algo_book/Practice/01_Greedy/05.py
+++ Algo_book/Practice/01_Greedy/05.py
@@ -4,15 +4,15 @@ N, M = map(int,input().split())
 weight = list(map(int,input().split()))
 weight.sort()
 result = len(list(combinations(weight,2)))
-print(result)
 count = 1
 for i in range(1,len(weight)):
     if weight[i] == weight[i-1]:
         count += 1
     elif count >= 2:
-        result -= len(list(combinations(count,2)))
+        count_list=[i for i in range(count)]
+        result -= len(list(combinations(count_list,2)))
         count = 1
     else:
-        continue
+        count = 1
 
 print(result)
