f634fff14783cce224252ff1b9ba348a89d972cd
mallang7 <seonug0725@gmail.com>
2022-03-25 21:22:15 +0900

    Update 07.py

--- Algo_book/Practice/02_Implementation/07.py
+++ Algo_book/Practice/02_Implementation/07.py
@@ -1,6 +1,10 @@
 # 럭키 스트레이트
 def solution(N):
-    
+    N_str = str(N)
+    left = N_str[:len(N_str)//2]
+    right = N_str[len(N_str)//2:]
+    if sum(left) == sum(right):
+        return "LUCKY"
     return "READY"
 
 print(solution(123402))
