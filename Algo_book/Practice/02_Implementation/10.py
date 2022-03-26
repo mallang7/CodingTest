40dee4a64626f36e16d78e283d5fe31433ab98a0
mallang7 <seonug0725@gmail.com>
2022-03-26 17:55:52 +0900

    Create 10.py

---
+++ Algo_book/Practice/02_Implementation/10.py
@@ -0,0 +1,37 @@
+# 자물쇠와 열쇠 325p
+def turn(key):
+    m = len(key)
+    key_turned=[[0]*m for _ in range(m)]
+    for i in range(m):
+        for j in range(m):
+            key_turned[j][m-i-1] = key[i][j]
+    return key_turned
+    
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length * 2):
+        for j in range(lock_length, lock_length *2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    new_lock = [[0] * (n * 3) for _ in range(n*3)]
+    for i in range(n):
+        for j in range(n):
+            new_lock[i+n][j+n] = lock[i][j]
+    for rotation in range(4):
+        key = turn(key)
+        for x in range(n * 2):
+            for y in range(n * 2):
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x +i][y + j] += key[i][j]
+                if check(new_lock) == True:
+                    return True
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x+i][y+j] -= key[i][j]
+    return False
