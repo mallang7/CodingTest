5171d3e0fb187094652d15e40433791493b61f8d
mallang7 <seonug0725@gmail.com>
2022-03-26 21:52:36 +0900

    Create 15.py

---
+++ Algo_book/Practice/03_DFS/BFS/15.py
@@ -0,0 +1,28 @@
+# 15. 특정 거리의 도시 찾기 339p
+from collections import deque
+
+n, m, k, x = map(int,input().split())
+
+graph=[[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+
+distance = [-1] * (n+1)
+distance[x] = 0
+queue = deque([x])
+
+while queue:
+    now = queue.popleft()
+    for i in graph[now]:
+        if distance[i] == -1:
+            distance[i] = distance[now] + 1
+            queue.append(i)
+check = False
+for i in range(1,n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+if check == False:
+    print(-1)
+    
